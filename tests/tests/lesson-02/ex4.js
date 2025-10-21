2308bb9ed2a74f7c103409cdfa8fd0d3038424a2
tramnguyen <quynhtram.tgdd.tn@gmail.com>
2025-10-21 12:09:42 +0700

    chore: fix excercises

--- tests/tests/lesson-02/ex4.js
+++ tests/tests/lesson-02/ex4.js
@@ -2,6 +2,4 @@ let height = 165;
 let expweight = 65 * 9 / 10;
 let maxweight = height - 100;
 let minweight = (height - 100) * 8 / 10;
-console.log("Cân nặng lý tưởng " + expweight + "kg");
-console.log("Cân nặng tối đa " + maxweight + "kg");
-console.log("Cân nặng tối thiểu " + minweight + "kg");
+console.log("Cân nặng lý tưởng " + expweight + "kg" + ", Cân nặng tối đa " + maxweight + "kg" + ", Cân nặng tối thiểu " + minweight + "kg");
